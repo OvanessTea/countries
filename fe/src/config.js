278051b1758d24e6530d4f501a00e244626b0728
OvanessTea <OvanessTea@yandex.com>
2025-05-24 15:06:43 +0200

    add code for FE

---
+++ fe/src/config.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'https://localhost:3000/countries/';
+
+export const ALL_COUNTRIES =
+  BASE_URL;
+
+export const searchByCountry = (name) => BASE_URL + 'name/' + name;
+
+export const filterByCode = (codes) =>
+  BASE_URL + 'alpha?codes=' + codes.join(',');
